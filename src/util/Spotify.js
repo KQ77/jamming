23dd577ea4381ff78152b0bd1883f73a8cd35655
KQ77 <katequinn7@gmail.com>
2020-05-09 20:49:44 -0400

    "fix logic with dyn redirectURI"

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,12 +1,14 @@
 let accessToken;
 // const redirectURI = encodeURI('https://jammy-jams.surge.sh/');
 
-let redirectURI; 
-if (window.location.href === `https://jammy-jams.surge.sh`) {
+let redirectURI = encodeURI(`${window.location.href}`)
+/*if (window.location.href === `https://jammy-jams.surge.sh`) {
  redirectURI = encodeURI('https://jammy-jams.surge.sh');
 } else {
     redirectURI = `http://localhost:3001`;
 }
+*/
+
 const id= '186f4ea56a7f427a8c9f7eb8d6ee8e1d';
 
 export const Spotify = {
